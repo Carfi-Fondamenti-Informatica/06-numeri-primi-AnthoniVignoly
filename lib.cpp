f16aa9bb60625bf4a8c1086d2a32290d904898e5
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-12-03 11:27:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+bool primo(int numero, int i) {
+  int resto=numero%i;
+  
+  if(numero<=i)returb true;
+  if(resto==0&&i>1)return false;
+  if(resto!=0) {
+    i++;
+    primo(numero, i);
+  }
+  else return true; 
+}
