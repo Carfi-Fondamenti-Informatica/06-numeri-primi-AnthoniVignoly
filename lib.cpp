a3344227530ed49ee9dfa6d3c11b81cf3c7bb32b
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-12-03 11:57:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool primo(int numero, int i) {
   int resto=numero%i;
   
-  if(numero<=i)returb true;
+  if(numero<=i)return true;
   if(resto==0&&i>1)return false;
   if(resto!=0) {
     i++;
